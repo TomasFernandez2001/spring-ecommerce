ae7c93be96718efb9df40aca8dcc2232163e955a
TomasFernandez2001 <totoloco392@gmail.com>
2024-09-03 18:18:28 -0300

    agregamos carrito

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,16 +1,27 @@
 package com.ecommerce.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Producto;
 import com.ecommerce.service.ProductoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
 	private ProductoService productoService;
 	
@@ -19,4 +30,23 @@ public class HomeController {
     	model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
     }
+   
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id, Model model) {
+    	log.info("id enviado como parametro{} ", id);
+    	Producto producto = new Producto();
+    	Optional<Producto> productoOptional= productoService.get(id);
+    	producto = productoOptional.get();
+    	
+    	model.addAttribute("producto", producto);
+    	return "usuario/productohome";
+    	
+    }
+    @PostMapping("/cart")
+    public String addCart() {
+       
+        
+        return "usuario/carrito.html";
+    }
+    
 }
